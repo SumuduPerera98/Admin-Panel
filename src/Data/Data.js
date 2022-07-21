0dbdf82378495278f1e7f3f410be4909c3080912
SumuduPerera98 <sharshiperera20@gmail.com>
2022-07-21 20:09:22 +0530

    sidebar - menu

---
+++ src/Data/Data.js
@@ -0,0 +1,35 @@
+//Sidebar imports
+import{
+    UilEstate,
+    UilClipboardAlt,
+    UilUsersAlt,
+    UilPackage,
+    UilChart,
+    UilSignOutAlt,
+} from "@iconscout/react-unicons";
+
+//Sidebar data
+export const SidebarData = [
+    {
+        icon: UilEstate,
+        heading: "Dashboard",
+    },
+    {
+        icon: UilClipboardAlt,
+        heading: "Orders",
+    },
+    {
+        icon: UilUsersAlt,
+        heading: "Customers",
+    },
+    {
+        icon: UilPackage,
+        heading: "Products",
+    },
+    {
+        icon: UilChart,
+        heading: "Analytics",
+    },
+   
+
+]
--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,15 +1,44 @@
-import React from "react"
+import React, {useState} from "react"
 import Logo from '../../imgs/logo.png'
 import './Sidebar.css'
 
+import { SidebarData } from "../../Data/Data"
+import {UilSignOutAlt} from '@iconscout/react-unicons'
+
 const Sidebar=()=>{
+
+    const [selected, setSelected]= useState(0)
+
     return(
         <div className="Sidebar">
+            {/* logo */}
             <div className="logo">
                 <img src={Logo} alt="" />
                 <span>
                     Sh<span>o</span>ps
                 </span>
+            </div>
+
+            {/* menu */}
+            <div className="menu">
+                    {SidebarData.map((item,index)=>{
+
+                        return(
+                            <div className={selected===index?'menuItem active': 'menuItem'}
+                            key={index}
+                            onClick={()=>setSelected(index)}
+                            >
+                                <item.icon/>
+                                <span>
+                                    {item.heading}
+                                </span>
+                            </div>   
+                            )
+                    })}
+                    <div className="menuItem">
+                        <UilSignOutAlt/>
+                    </div>
+                
             </div>
         </div>
     )
