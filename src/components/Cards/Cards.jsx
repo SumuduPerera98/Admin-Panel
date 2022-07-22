7da29eb078d7a32496ad5d387731ea0f512606ba
SumuduPerera98 <sharshiperera20@gmail.com>
2022-07-22 13:16:41 +0530

    adding cards

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,9 +1,27 @@
 import React from 'react'
+import { CardsData } from '../../Data/Data'
+import Card from '../Card/Card'
 import './Cards.css'
 
 const Cards = () => {
   return (
-    <div>Cards</div>
+    <div className='Cards'>
+        {CardsData.map((card,id)=>{
+            return(
+                <div className='parentContainer'>
+                    <Card
+                    title={card.title}
+                    color={card.color}
+                    barValue={card.barValue}
+                    value={card.value}
+                    png={card.png}
+                    series={card.series}
+                    />
+                </div>
+            )
+        })}
+    
+    </div>
   )
 }
 
