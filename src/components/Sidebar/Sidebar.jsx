ae58ffd1794c7a1b7f709fb361cdfc995a475f73
SumuduPerera98 <sharshiperera20@gmail.com>
2022-09-05 14:19:43 +0530

    make responsive for mobile view

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -3,14 +3,36 @@ import Logo from '../../imgs/logo.png'
 import './Sidebar.css'
 
 import { SidebarData } from "../../Data/Data"
-import {UilSignOutAlt} from '@iconscout/react-unicons'
-
+import {UilSignOutAlt, UilBars} from '@iconscout/react-unicons'
+import {motion} from 'framer-motion'
 const Sidebar=()=>{
 
     const [selected, setSelected]= useState(0)
+    const [expanded, setExpanded]= useState(true)
+
+    const sidebarVarients={
+        true:{
+            left:'0'
+        },
+        false:{
+            left:'-60%'
+        }
+    }
 
     return(
-        <div className="Sidebar">
+        <>
+        <div className="bars" style={expanded? {left: "60%"}:{
+            left:"5%"
+        }}
+        onClick={()=>setExpanded(!expanded)}
+        >
+                <UilBars/>
+        </div>
+        <motion.div className="Sidebar" 
+        variants={sidebarVarients}
+        animate={window.innerWidth<=768?`${expanded}`:''}
+        >
+            
             {/* logo */}
             <div className="logo">
                 <img src={Logo} alt="" />
@@ -40,7 +62,8 @@ const Sidebar=()=>{
                     </div>
                 
             </div>
-        </div>
+        </motion.div>
+        </>
     )
 }
 
